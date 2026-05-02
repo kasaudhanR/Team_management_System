8c30e29ee850de1bf7ae98a020be85f9f221515f
kasaudhanR <kasaudhanjp8@gmail.com>
2026-05-02 15:59:47 +0530

    frontend completed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", 
+  baseURL: "https://team-management-system-orxl.onrender.com/api", 
   withCredentials: false
 });
 
